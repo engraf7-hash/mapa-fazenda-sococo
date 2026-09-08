5c972e33abb9d8d32abd7efcf9c86e4fa4288e8b
engraf7-hash <eng.raf7@gmail.com>
2026-09-08 01:23:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  - Ladrilhos de mapa (OpenStreetMap / Esri) -> cache-first "cresce com o uso":
 //    todo ladrilho visitado com internet fica salvo e volta a ser servido offline.
 
-const CACHE_NAME = 'campo-fazenda-v11';
+const CACHE_NAME = 'campo-fazenda-v17';
 const TILE_CACHE = 'campo-fazenda-tiles-v1';
 
 const APP_SHELL = [
